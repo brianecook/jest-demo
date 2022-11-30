e7b629b8f459814d1cfc4b8fa06ad7d2eec62a7b
Brian Cook <brianecook@gmail.com>
2022-11-30 08:59:26 -0800

    adds some simple tests

---
+++ formatMoney.test.js
@@ -0,0 +1,13 @@
+const formatMoney = require("./formatMoney")
+
+describe('formatMoney function', () => {
+  it ('works with fractional numbers', () => {
+    expect(formatMoney(1)).toEqual('$0.01');
+    expect(formatMoney(140)).toEqual('$1.40');
+  });
+
+  it ('leaves off cents when its whole dollars', () => {
+    expect(formatMoney(5000)).toEqual('$50');
+    expect(formatMoney(100)).toEqual('$1');
+  });
+})
